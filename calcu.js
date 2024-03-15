c6a702530a1f39ef366c6df17a3cb15ea0112982
Rushikesh Sanjay Sharma <163033185+RushikeshSharma@users.noreply.github.com>
2024-03-15 15:41:29 +0530

    calcu.js

--- calcu.js
+++ calcu.js
@@ -30,21 +30,15 @@ function reset() {
   updateDisplay();
 }
 function calculatePercentage() {
-  var displayValue = document.getElementById('display').value;
-
- 
-  var number = parseFloat(displayValue);
-
-  
-  var percentage = number / 100;
-
-  
-  document.getElementById('display').value = percentage;
+  try {
+    currentInput = (eval(currentInput) / 100).toString();
+    updateDisplay();
+  } catch (error) {
+    display.value = 'Error';
+    setTimeout(reset, 1000);
+  }
 }
-function openEngineeringCalculator() {
- 
-  document.getElementById('engineering-calculator').style.display = 'block';
-}
+
 function deleteLast() {
   currentInput = currentInput.slice(0, -1);
   if (currentInput === '') {
@@ -66,23 +60,4 @@ function calculate() {
 function updateDisplay() {
   display.value = currentInput;
 }
-function calculate214() {
-  var displayValue = document.getElementById('display').value;
-  var result = eval(displayValue); // Evaluate the expression
 
-  // Add the calculation and result to the history
-  addToHistory(displayValue + ' = ' + result);
-
-  // Display the result (you might want to update your calculator interface to handle this)
-  console.log(result);
-}
-function showHistory() {
-  var historyDisplay = document.getElementById('history-display');
-  historyDisplay.innerHTML = ''; // Clear previous content
-
-  for (var i = 0; i < historyList.length; i++) {
-      var listItem = document.createElement('li');
-      listItem.textContent = historyList[i];
-      historyDisplay.appendChild(listItem);
-  }
-}
